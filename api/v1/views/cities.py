11fd9bbb21fbac5ff409ad06914646c0206ec250
odelolajosh <oladapoodelola@gmail.com>
2022-10-24 18:01:46 +0100

    feat: CRUD for cities

---
+++ api/v1/views/cities.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+""" handles all CRUD for cities """
+
+from email.policy import strict
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+
+
+@app_views.route("/", strict_slashes=False, methods=["GET"])
+def get_cities():
+    """ return all cities """
+    cities = storage.all("City")
+    return jsonify([city.to_dict() for city in cities.values()])
+
+
+@app_views.route("/cities/<city_id>", strict_slashes=False, methods=["GET"])
+def get_city(city_id):
+    """ return a city by id """
+    city = storage.get("City", city_id)
+    if city is None:
+        abort(404)
+    return jsonify(city.to_dict())
+
+
+@app_views.route("/cities/<city_id>", strict_slashes=False, methods=["DELETE"])
+def delete_city(city_id):
+    """ delete a city by id """
+    city = storage.get("City", city_id)
+    if city is None:
+        abort(404)
+    storage.delete(city)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route("/states/<state_id>/cities",
+                 strict_slashes=False,
+                 methods=["POST"])
+def post_city(state_id):
+    """ create a city """
+    if not request.get_json():
+        abort(400, "Not a JSON")
+    if "name" not in request.get_json():
+        abort(400, "Missing name")
+    city = City(**request.get_json())
+    city.state_id = state_id
+    storage.new(city)
+    storage.save()
+    return jsonify(city.to_dict()), 201
+
+
+@app_views.route("/cities/<city_id>", strict_slashes=False, methods=["PUT"])
+def put_city(city_id):
+    """ update a city """
+    city = storage.get("City", city_id)
+    if city is None:
+        abort(404)
+    if not request.get_json():
+        abort(400, "Not a JSON")
+    for key, value in request.get_json().items():
+        if key not in ["id", "created_at", "updated_at", "state_id"]:
+            setattr(city, key, value)
+    storage.save()
+    return jsonify(city.to_dict()), 200
