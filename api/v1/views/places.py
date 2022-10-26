53f41382a05ac8615237b5718dc3ed936a505ebb
odelolajosh <oladapoodelola@gmail.com>
2022-10-26 03:49:33 +0100

    chore: document

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""handle CRUD for places"""
+"""handles all CRUD RestFul API actions for places"""
 
 from api.v1.views import app_views
 from flask import jsonify, request, abort
